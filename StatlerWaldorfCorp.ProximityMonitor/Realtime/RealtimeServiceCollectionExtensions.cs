0d1dc894a77e209c1822d14317d606438d9971e0
Erbynn John <john.erbynn@gmail.com>
2024-06-27 21:28:29 -0500

    add RealtimeServiceCollectionExtensions

--- StatlerWaldorfCorp.ProximityMonitor/Realtime/RealtimeServiceCollectionExtensions.cs
+++ StatlerWaldorfCorp.ProximityMonitor/Realtime/RealtimeServiceCollectionExtensions.cs
@@ -2,11 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PubnubApi;
 
 namespace StatlerWaldorfCorp.ES_CQRS_ProximityMonitor.Realtime
 {
     public static class RealtimeServiceCollectionExtensions
     {
-        
+        public static IServiceCollection AddRealtimeService(this IServiceCollection services)
+        {
+            services.AddTransient<PubnubFactory>();
+            
+            return AddInternal(services, p => p.GetRequiredService<PubnubFactory>(), ServiceLifetime.Singleton);
+        }
+
+        private static IServiceCollection AddInternal(
+            this IServiceCollection serviceCollection, 
+            Func<IServiceProvider, PubnubFactory> factoryProvider,
+            ServiceLifetime lifetime)
+        {
+            Func<IServiceProvider, object> factoryFunc = (provider) => {
+                var factory = factoryProvider(provider);
+                return factory.CreatePubnub();
+            };   
+
+            var descriptor = new ServiceDescriptor(typeof(Pubnub), factoryFunc, lifetime);
+            serviceCollection.Add(descriptor);
+            return serviceCollection;
+        } 
     }
 }
