d4f1e9682e1cbaefabf927b495300d0e825dbf92
Erbynn John <john.erbynn@gmail.com>
2024-06-27 21:16:28 -0500

    update realtiime configuration

---
+++ StatlerWaldorfCorp.ProximityMonitor/Realtime/PubnubRealtimePublisher.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PubnubApi;
+
+namespace StatlerWaldorfCorp.ES_CQRS_ProximityMonitor.Realtime
+{
+    public class PubnubRealtimePublisher : IRealtimePublisher
+    {
+        private ILogger logger;
+        private Pubnub pubnubClient;
+
+        public PubnubRealtimePublisher(ILogger<PubnubRealtimePublisher> logger, Pubnub pubnubClient)
+        {
+            this.logger = logger;
+            this.pubnubClient = pubnubClient;
+        }
+
+        /// <summary>
+        /// This calls the Time method on the pubnubClient instance of the Pubnub class. 
+        /// This method is used to get the current time from the Pubnub servers to check if its up or down
+        /// </summary>
+        public async Task ValidateAsync()
+        {
+             var result = await pubnubClient.Time().ExecuteAsync();
+            if (result.Status.Error)
+            {
+                logger.LogError($"Unable to connect to Pubnub: {result.Status.ErrorData.Information}");
+                throw result.Status.ErrorData.Throwable;
+            }
+            else
+            {
+                logger.LogInformation("Pubnub connection established.");
+            }
+        }
+
+        public async Task PublishAsync(string channelName, string message)
+        {
+            PNResult<PNPublishResult> publishResponse = await pubnubClient.Publish()
+                                                                        .Channel(channelName)
+                                                                        .Message(message)
+                                                                        .ExecuteAsync();
+            // var response = publishResponse.Result;
+            PNStatus status = publishResponse.Status;
+            if (status.Error) {
+                logger.LogError($"Failed to publish on channel {channelName}: {status.ErrorData.Information}");
+                return;
+            }
+            logger.LogInformation($"Published message on channel {channelName}, {status.AffectedChannels.Count} affected channels, message: {message}, code: {status.StatusCode}");
+        }
+    }
+}
