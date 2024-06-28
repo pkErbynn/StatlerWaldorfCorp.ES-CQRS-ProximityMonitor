76e3b0158f1a73a0f832ee7932b8c43b45cf1c3b
Erbynn John <john.erbynn@gmail.com>
2024-06-28 16:31:06 -0500

    update realtimeserviceExtention

--- StatlerWaldorfCorp.ProximityMonitor/Realtime/RealtimeServiceCollectionExtensions.cs
+++ StatlerWaldorfCorp.ProximityMonitor/Realtime/RealtimeServiceCollectionExtensions.cs
@@ -28,6 +28,25 @@ namespace StatlerWaldorfCorp.ES_CQRS_ProximityMonitor.Realtime
             var descriptor = new ServiceDescriptor(typeof(Pubnub), factoryFunc, lifetime);
             serviceCollection.Add(descriptor);
             return serviceCollection;
-        } 
+        }
+
+        /// <summary>
+        /// A factory method is used to register the Pubnub instance. 
+        /// This method retrieves the PubnubFactory from the DI container and uses it to create a Pubnub instance.
+        /// </summary>
+        // public static IServiceCollection AddRealtimeService(this IServiceCollection services)
+        // {
+        //     // Register PubnubFactory as a singleton
+        //     services.AddSingleton<PubnubFactory>();
+
+        //     // Register Pubnub using a factory method
+        //     services.AddSingleton<Pubnub>(serviceProvider =>
+        //     {
+        //         var factory = serviceProvider.GetRequiredService<PubnubFactory>();
+        //         return factory.CreatePubnub();
+        //     });
+
+        //     return services;
+        // }
     }
 }
