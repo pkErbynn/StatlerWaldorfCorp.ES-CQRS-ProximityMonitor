fc9134e27b3a73bc1735176d0651e67edfa3c0f5
Erbynn John <john.erbynn@gmail.com>
2024-06-27 21:14:18 -0500

    update queue configuration

---
+++ StatlerWaldorfCorp.ProximityMonitor/Queues/QueueOptionSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatlerWaldorfCorp.ES_CQRS_ProximityMonitor.Queues
+{
+    public class QueueOptionSettings
+    {
+        public string ProximityDetectedEventQueueName { get; set; }
+    }
+}
